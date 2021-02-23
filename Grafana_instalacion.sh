3915f395c06c97e18d528d30e156862b49b4512b
Cristhian-repo <79467668+Cristhian-repo@users.noreply.github.com>
2021-02-23 17:55:15 -0500

    Update Grafana_instalacion.sh

--- Grafana_instalacion.sh
+++ Grafana_instalacion.sh
@@ -4,7 +4,7 @@ yum update
 
 > /etc/yum.repos.d/grafana.repo
 
-echo -e "[grafana] \nname=grafana \nbaseurl=https://packages.grafana.com/oss/rpm \nrepo_gpgcheck=1 \nenabled=1 \ngpgcheck=1 \ngpgkey=https://packages.grafana.com/gpg.key \nsslverify=1 \nsslcacert=/etc/pki/tls/certs/ca-bundle.crt" >> /etc/yum.repos.d/grafana.repo
+echo -e "[grafana] \nname=grafana \nbaseurl=https://packages.grafana.com/oss/rpm \nrepo_gpgcheck=1 \nenabled=1 \ngpgcheck=1 \ngpgkey=https://packages.grafana.com/gpg.key \nsslverify=1 \nsslcacert=/etc/pki/tls/certs/ca-bundle.crt" > /etc/yum.repos.d/grafana.repo
 
 yum update
 
