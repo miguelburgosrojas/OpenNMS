f3dc88937aa09a71cc18ba90379673e6529d07d4
Cristhian-repo <79467668+Cristhian-repo@users.noreply.github.com>
2021-02-23 10:27:43 -0500

    Update and rename Grafana_instalación to Grafana_instalacion.sh
    Comandos ejecutados para distribución linux Centos7

---
+++ Grafana_instalacion.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+yum update
+
+cat >> /etc/yum.repos.d/grafana.repo
+#se ingresan las siguientes lineas en el editor
+[grafana]
+name=grafana
+baseurl=https://packages.grafana.com/oss/rpm
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+
+yum update
+
+yum install grafana
+
+systemctl start grafana-server
+systemctl enable grafana-server
+
+
+
