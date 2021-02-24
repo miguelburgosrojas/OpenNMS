dffe514936277e151dc89e4b71b06c35cf88f36b
miguelburgosrojas <33735076+miguelburgosrojas@users.noreply.github.com>
2021-02-24 17:35:17 -0500

    Update Elasticsearch_install.sh

--- Elasticsearch_install.sh
+++ Elasticsearch_install.sh
@@ -4,7 +4,7 @@ touch elasticsearch.repo
 ELASTICSEACH_VERSION="7.11.1"
 printf "[elasticsearch]\n" >> elasticsearch.repo
 printf "name=Elasticsearch repository for ${ELASTICSEACH_VERSION} packages\n" >> elasticsearch.repo
-printf "baseurl=https://artifacts.elastic.co/packages/{ELASTICSEACH_VERSION}/yum\n" >> elasticsearch.repo
+printf "baseurl=https://artifacts.elastic.co/packages/${ELASTICSEACH_VERSION}/yum\n" >> elasticsearch.repo
 printf "gpgcheck=1\n" >> elasticsearch.repo
 printf "gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch\n" >> elasticsearch.repo
 printf "enabled=0\n" >> elasticsearch.repo
